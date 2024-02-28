4c96ca06a117ef6b68d0bd1ff625162da86604a5
nikiasek <nikiasek50@gmail.com>
2024-02-28 21:44:33 +0100

    progress
    exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    hovinko = ""
+    if(Math.sign(count) == 1) {
+        for(i = 0; i < count; i++) {
+            hovinko += word
+        }
+        return hovinko
+    }else if(Math.sign(count) == 0) {
+        return ""
+    }else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
